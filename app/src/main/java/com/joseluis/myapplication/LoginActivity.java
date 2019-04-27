b4f5bafe4a0ae1976b3d0ba1651a0ab6f5a822cd
MelannyRimache <mell.rc1621@gmail.com>
2019-04-27 17:43:06 -0500

    Commit 27-04 17:41

--- app/src/main/java/com/joseluis/myapplication/LoginActivity.java
+++ app/src/main/java/com/joseluis/myapplication/LoginActivity.java
@@ -2,6 +2,7 @@ package com.joseluis.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goLoginAccount(View view) {
+    }
+
+    public void goCreateAccount(View view) {
     }
 }
